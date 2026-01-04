3adaa34a1984fad37660741167cf9626cf9f1077
范方忆 <fan750798@gmail.com>
2026-01-04 23:22:28 +0800

    Update variables1.rs

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -1,12 +1,6 @@
-// variables1.rs
-//
-// Make me compile!
-//
-// Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
-// hint.
-
-
 fn main() {
+    // 1. 定义变量x，绑定值5（Rust会自动推导x的类型为i32）
     let x = 5;
+    // 2. 打印变量x的值，{}是占位符，会被x的实际值替换
     println!("x has the value {}", x);
 }
