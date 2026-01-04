e989eb19918ebd9fae648afed7fbcfa84afcf3e4
范方忆 <fan750798@gmail.com>
2026-01-04 18:03:46 +0800

    Update generics1.rs

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -1,14 +1,5 @@
-// generics1.rs
-//
-// This shopping list program isn't compiling! Use your knowledge of generics to
-// fix it.
-//
-// Execute `rustlings hint generics1` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+    // 替换通配符 ? 为具体的类型 &str（字符串切片），匹配 push 的值类型
+    let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
 }
