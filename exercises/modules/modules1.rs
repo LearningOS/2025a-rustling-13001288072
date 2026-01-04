f3a2aa079fd9b847e7b27aa5772d0b45de564cef
范方忆 <fan750798@gmail.com>
2026-01-04 21:07:42 +0800

    Update modules1.rs

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -1,17 +1,11 @@
-// modules1.rs
-//
-// Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+    // 将 make_sausage 函数设为公有，允许模块外部调用
+    pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
