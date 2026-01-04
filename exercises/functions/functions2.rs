2c11ffb5fd7983b24d298338e52a08999d8793c9
范方忆 <fan750798@gmail.com>
2026-01-04 18:01:27 +0800

    Update functions2.rs

--- exercises/functions/functions2.rs
+++ exercises/functions/functions2.rs
@@ -1,13 +1,8 @@
-// functions2.rs
-//
-// Execute `rustlings hint functions2` or use the `hint` watch subcommand for a
-// hint.
-
-
 fn main() {
     call_me(3);
 }
 
+// 函数接收 i32 类型参数 num，循环打印指定次数的呼叫信息
 fn call_me(num: i32) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
