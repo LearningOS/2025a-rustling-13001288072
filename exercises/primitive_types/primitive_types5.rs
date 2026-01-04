ed9f700084f939ee949797757a0751566a1a335c
范方忆 <fan750798@gmail.com>
2026-01-04 22:28:07 +0800

    Update primitive_types5.rs

--- exercises/primitive_types/primitive_types5.rs
+++ exercises/primitive_types/primitive_types5.rs
@@ -1,14 +1,6 @@
-// primitive_types5.rs
-//
-// Destructure the `cat` tuple so that the println will work.
-//
-// Execute `rustlings hint primitive_types5` or use the `hint` watch subcommand
-// for a hint.
-
-
 fn main() {
     let cat = ("Furry McFurson", 3.5);
-    let /* your pattern here */(name, age) = cat;
+    let (name, age) = cat; // 解构元组，将两个元素分别绑定到name和age变量
 
     println!("{} is {} years old.", name, age);
 }
