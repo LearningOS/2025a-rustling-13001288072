739f71d9886a56821e46ce459180a92c03cf977c
范方忆 <fan750798@gmail.com>
2026-01-04 17:52:30 +0800

    Update enums1.rs

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -1,17 +1,15 @@
-// enums1.rs
-//
-// No hints this time! ;)
-
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
-    // TODO: define a few types of messages as used below
+    // 定义题目中用到的四个枚举变体
+    Quit,
+    Echo,
+    Move,
+    ChangeColor,
 }
 
 fn main() {
-    println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Quit);          // 输出 Quit
+    println!("{:?}", Message::Echo);          // 输出 Echo
+    println!("{:?}", Message::Move);          // 输出 Move
+    println!("{:?}", Message::ChangeColor);   // 输出 ChangeColor
 }
