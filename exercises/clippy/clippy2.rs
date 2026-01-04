99623f9dfff36c9681ccb705564f8088fa8767ca
范方忆 <fan750798@gmail.com>
2026-01-04 17:41:01 +0800

    Update clippy2.rs

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -1,15 +1,11 @@
-// clippy2.rs
-// 
-// Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    for x in option {
+    
+    // 替换冗余的 for 循环，用 if let 匹配 Option 中的值
+    if let Some(x) = option {
         res += x;
     }
+    
     println!("{}", res);
 }
